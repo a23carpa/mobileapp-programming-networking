9212205558dbec09c3c53133a5dae5298c6fb09d
a23carpa <a23carpa@student.his.se>
2024-04-22 15:43:38 +0200

    Create new List from Json file and send it to update method in adapter. Then update adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,37 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+    private List<Mountain> list;
+    private RecyclerViewAdapter adapter;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
-                new Mountain("Matterhorn"),
-                new Mountain("Mont Blanc"),
-                new Mountain("Denali"),
-                new Mountain("Matterhorn"),
-                new Mountain("Mont Blanc"),
-                new Mountain("Denali"),
-                new Mountain("Matterhorn"),
-                new Mountain("Mont Blanc"),
-                new Mountain("Denali"),
-                new Mountain("Matterhorn"),
+        list = new ArrayList<>(Arrays.asList(
+                new Mountain("XX"),
                 new Mountain("Mont Blanc"),
                 new Mountain("Denali")
         ));
 
-
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+         adapter = new RecyclerViewAdapter(this, list, new RecyclerViewAdapter.OnClickListener() {
             @Override
             public void onClick(Mountain item) {
                 Log.d("button", "Button clicked");
@@ -48,13 +46,19 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
 
-
+        gson = new Gson();
+        //new JsonTask(this).execute(JSON_URL);
         new JsonFile(this, this).execute(JSON_FILE);
+
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> listOfMountains = gson.fromJson(json, type);
+        adapter.update(listOfMountains);
+        adapter.notifyDataSetChanged();
+
     }
 
 }
